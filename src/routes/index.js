b279f098fd7e8b259cb92004d335448eec3981de
sahil dhiman <194502@nith.ac.in>
2024-10-27 20:52:44 +0530

    change naming conventions for routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const apiv1Routes = require('./v1/index.js');
+const v1Routes = require('./v1/index.js');
 
-router.use('/v1',apiv1Routes);
+router.use('/v1',v1Routes);
 
 module.exports = router;
